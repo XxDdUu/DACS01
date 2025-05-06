543284903f2a75f9af28324236e2a63eda6ad13b
HoangLong-88 <quoccuong130406@gmail.com>
2025-05-06 14:31:52 +0700

    halo

--- Backend/Controller/MainController.py
+++ Backend/Controller/MainController.py
@@ -10,7 +10,7 @@ class MainController:
 
         self.login_window.switch_to_register = self.show_register
         self.register_window.switch_to_login = self.show_login
-        self.login_window.switch_to_dashboardApp = self.show_dashboardApp()
+        self.login_window.switch_to_dashboardApp = self.show_dashboardApp
 
         self.show_login()
         self.employer_controller = EmployerController(self.register_window)
