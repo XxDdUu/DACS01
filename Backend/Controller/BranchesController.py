8f1bf7474fb46de7b960c167b90d09963d8f1d39
HoangLong-88 <quoccuong130406@gmail.com>
2025-05-11 16:43:29 +0700

    update remove product + branches

--- Backend/Controller/BranchesController.py
+++ Backend/Controller/BranchesController.py
@@ -7,11 +7,21 @@ class ManageBranches:
         self.distribution_view = distribution_view
         self.branches_dao = BranchesDAO()
         self.distribution_view.branch_add_btn.clicked.connect(self.handle_add_button)
+        self.distribution_view.branch_remove_btn.clicked.connect(self.handle_remove_button)
     def handle_add_button(self):
         data = self.distribution_view.BFD.get_branchesForm_data()
         print("[DEBUG] Form data:", data)  # Kiểm tra lại giá trị thực tế
-        self.branches_dao.insert_branches(data)
+        # self.branches_dao.insert_branches(data)
         success, message = self.branches_dao.insert_branches(data)
+        if success:
+            QMessageBox.information(self.distribution_view, "Success", message)
+        else:
+            QMessageBox.warning(self.distribution_view, "Error", message)
+    def handle_remove_button(self):
+        data = self.distribution_view.BFD.get_branchesForm_data()
+        print("[DEBUG] Form data:", data)  # Kiểm tra lại giá trị thực tế
+        # self.branches_dao.remove_branches(data)
+        success, message = self.branches_dao.remove_branches(data)
         if success:
             QMessageBox.information(self.distribution_view, "Success", message)
         else:
