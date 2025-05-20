0384395a2dfb04311791ee7d028a5aa0b26d3629
XxDdUu <XxDdUu>
2025-05-20 15:56:45 +0700

    ui & login

--- Backend/Model/Employer.py
+++ Backend/Model/Employer.py
@@ -1,7 +1,8 @@
 class Employer:
-    def __init__(self, ID, username, email, phone_number, date_of_birth, enterprise_id):
+    def __init__(self, ID, username, create_at_time,email, phone_number, date_of_birth, enterprise_id):
         self.ID = ID
         self.username = username
+        self.create_at_time = create_at_time
         self.email = email
         self.phone_number = phone_number
         self.date_of_birth = date_of_birth
