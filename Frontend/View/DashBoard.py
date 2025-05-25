c02b5e72cacd5f350d984f1691be09b939615728
Duy <tranquangduy15122006@gmail.com>
2025-05-25 16:45:33 +0700

    enterprise data -> ui

--- Frontend/View/DashBoard.py
+++ Frontend/View/DashBoard.py
@@ -1,16 +1,17 @@
 from PyQt6 import uic, QtWidgets, QtGui
 from PyQt6.QtGui import QIcon, QStandardItemModel, QStandardItem
-from PyQt6.QtWidgets import QApplication, QMainWindow, QAbstractSpinBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QAbstractSpinBox, QMessageBox
 from PyQt6.QtCore import Qt, QPoint, QEvent, QDate, QPropertyAnimation, QEasingCurve, QRect
 import sys
 import Frontend.View.resources_rc
 from datetime import datetime
 
 class DashBoard(QMainWindow):
-	def __init__(self, employer_data, controller):
+	def __init__(self, controller ,employer_data = None, enterprise_data = None):
 		super().__init__()
 		self.controller = controller
 		self.employer_data = employer_data
+		self.enterprise_data = enterprise_data
 		uic.loadUi("Frontend/dashboardUI.ui", self)
 		self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
 		self.setWindowIcon(QIcon("Frontend/View/img/logo/dark_pythonLogo.png"))
@@ -26,7 +27,6 @@ class DashBoard(QMainWindow):
 		self.header.mouseMoveEvent = self.mouse_move_event
 		self.header.mousePressEvent = self.mouse_press_event
 		self.header.mouseReleaseEvent = self.mouse_release_event
-		self.sale_qdate_edit.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons) #làm mất nút up-down của qDateEdit
 
 		self.fetch_account_info(employer_data)
 		self.active_switch_pages()
@@ -38,8 +38,7 @@ class DashBoard(QMainWindow):
 		self.left_menu_animation.setEasingCurve(QEasingCurve.Type.InOutQuart)
 		self.btn_toggle_menu.clicked.connect(self.toggle_menu)
 
-		self.switch_to_login = None
-		self.logout_label.clicked.connect(self.handle_logOut_btn)
+		self.logout_label.clicked.connect(self.confirm_logout)
 		self.display_PS_table()
 
 	def exit_window(self):
@@ -75,7 +74,18 @@ class DashBoard(QMainWindow):
 		qbirthdate = QDate(birthdate.year, birthdate.month, birthdate.day)
 
 		self.dateEdit.setDate(qbirthdate)
+
+	def fetch_enterprise_info(self, enterprise_data):
+		self.enterprise_name_line_edit.setText(enterprise_data.name)
+		self.enterprise_founder_line_edit.setText(enterprise_data.founder)
+		self.enterprise_address_line_edit.setText(enterprise_data.address)
+		self.enterprise_phone_num_line_edit.setText(enterprise_data.phone_number)
+		self.enterprise_bs_type_text_edit.setText(enterprise_data.type)
+		self.enterprise_bs_type_text_edit.setAlignment(Qt.AlignmentFlag.AlignCenter)
+		self.enterprise_industry_line_edit.setText(enterprise_data.industry)
+
 	def active_switch_pages(self):
+
 		self.main_display_widget.setCurrentWidget(self.Home_page)
 		self.home_btn.clicked.connect(lambda: self.main_display_widget.setCurrentWidget(self.Home_page))
 		self.account_setting_btn.clicked.connect(lambda: self.main_display_widget.setCurrentWidget(self.Account_setting_page))
@@ -84,20 +94,33 @@ class DashBoard(QMainWindow):
 		self.productSales_menu_btn.clicked.connect(lambda: self.main_display_widget.setCurrentWidget(self.Product_sale_page))
 		self.distribution_btn.clicked.connect(lambda: self.main_display_widget.setCurrentWidget(self.Distribution_page))
 
+		self.employer_and_enterpriseInfo.setCurrentWidget(self.employer_info)
+		self.btn_enterprise_info.clicked.connect(lambda: self.employer_and_enterpriseInfo.setCurrentWidget(self.enterprise_info))
+		self.btn_profile.clicked.connect(lambda: self.employer_and_enterpriseInfo.setCurrentWidget(self.employer_info))
+
 	def toggle_menu(self):
 		current_width = self.mainMenu.width()
 		new_width = 44 if self.menu_expanded else 150
-
+		if self.menu_expanded:
+			self.logout_label.setText('<img src="Frontend/View/img/icon/icons8_logout_rounded_down.svg">')
+		else:
+			self.logout_label.setText('<img src="Frontend/View/img/icon/icons8_logout_rounded_down.svg" style="width:16px; height:16px; vertical-align:middle;"> Log out')
+			
 		self.left_menu_animation.setStartValue(current_width)
 		self.left_menu_animation.setEndValue(new_width)
 		self.left_menu_animation.start()
 
 		self.menu_expanded = not self.menu_expanded
 
-	def handle_logOut_btn(self):
+	def handle_logout(self):
 		if self.switch_to_login:
 			self.switch_to_login()
 
+	def confirm_logout(self):
+		qm = QMessageBox
+		res = qm.question(self, 'Confirm', 'Are you sure to log out?', qm.StandardButton.Yes | qm.StandardButton.No)
+		if res == qm.StandardButton.Yes:
+			self.handle_logout()
 	def get_PSForm_data(self):
 		return {
 			"id": self.saleID_PS_le.text().strip(),
@@ -107,32 +130,6 @@ class DashBoard(QMainWindow):
 			"quantity_sold": self.quantitySold_PS_le.text().strip(),
 			"amount_total": self.saleAmount_PS_le.text().strip()
 		}
-	# def display_PS_table(self):
-	# 	productSale = []
-	# 	if hasattr(self.controller,'productSales_controller') and self.controller.productSales_controller:
-	# 		print("DEBUG employer ID:", self.employer_data.ID)
-	# 		print("DEBUG enterprise ID:", self.employer_data.enterprise_id)
-	# 		productSale = self.controller.get_PS_data(
-	# 			self.employer_data.ID,
-	# 			self.employer_data.enterprise_id
-	# 		)
-	# 		print("DEBUG result from DB:", productSale)
-	#
-	# 	model = QStandardItemModel(len(productSale),6)
-	# 	model.setHorizontalHeaderLabels(["SALE_ID", "Product_ID", "Branch_ID",
-	# 												  "SALE_DATE", "QUANTITY_SOLD", "SALE_AMOUNT"])
-	# 	for row_index, prod_sale in enumerate(productSale):
-	# 		model.setItem(row_index, 0, QStandardItem(prod_sale["SALE_ID"]))
-	# 		model.setItem(row_index, 1, QStandardItem(str(prod_sale["Product_ID"])))
-	# 		model.setItem(row_index, 2, QStandardItem(str(prod_sale["Branch_ID"])))
-	# 		model.setItem(row_index, 3, QStandardItem(prod_sale["SALE_DATE"]))
-	# 		model.setItem(row_index, 4, QStandardItem(prod_sale["QUANTITY_SOLD"]))
-	# 		model.setItem(row_index, 5, QStandardItem(prod_sale["SALE_AMOUNT"]))
-	# 	self.PS_data_table.setModel(model)
-	# 	self.PS_data_table.resizeColumnsToContents()
-	# 	print("Product Sales:", productSale)
-	#
-	# 	return self.PS_data_table
 
 	def display_PS_table(self):
 		productSale = []
@@ -199,3 +196,8 @@ class DashBoard(QMainWindow):
 
 		return self.PS_data_table
 
+		#setter method
+		def set_employer_data(self, employer_data):
+			self.employer_data = employer_data
+		def set_enterprise_data(self, enterprise_data):
+			self.enterprise_data = enterprise_data
