8abb0b3b715fec5796f7f646338580735b482ac6
Duy <tranquangduy15122006@gmail.com>
2025-05-28 22:20:55 +0700

    crud product

--- Frontend/View/Add_and_update_branch_product.py
+++ Frontend/View/Add_and_update_branch_product.py
@@ -21,14 +21,17 @@ class Add_and_update_branch_product(QtWidgets.QWidget):
 		self._drag_active = False
 		self._drag_position = QPoint()
 
-		self.branch_id_line_edit.setMaximumHeight(0)
 		self.branch_id_expanded = False
+
+		self.product_id_expanded = False
 		try:
-			self.update_and_add_label.clicked.disconnect()
+			self.product_update_and_add_label.clicked.disconnect()
+			self.branch_update_and_add_label.clicked.disconnect()
 		except TypeError:
 				pass
 		print("Connecting clicked signal now")
-		self.update_and_add_label.clicked.connect(self.on_update_and_add_label_clicked)
+		self.branch_update_and_add_label.clicked.connect(self.on_update_and_add_branch_label_clicked)
+		self.product_update_and_add_label.clicked.connect(self.on_update_and_add_product_label_clicked)
 
 	def toggle_maximize(self):
 		if self.is_maximized:
@@ -42,10 +45,12 @@ class Add_and_update_branch_product(QtWidgets.QWidget):
 		if res == qm.StandardButton.Yes:
 			self.close()
 
-	def on_update_and_add_label_clicked(self):
-		print("label clicked")
+	def on_update_and_add_branch_label_clicked(self):
 		self.toggle_branch_id()
-		self.switch_add_and_update()
+		self.switch_add_and_update_branch()
+	def on_update_and_add_product_label_clicked(self):
+		self.toggle_product_id()
+		self.switch_add_and_update_product()
 
 	def toggle_branch_id(self):
 		print("toggle_branch_id triggered")
@@ -57,18 +62,38 @@ class Add_and_update_branch_product(QtWidgets.QWidget):
 		self.branch_id_line_edit.setMaximumHeight(new_height)
 
 		self.branch_id_expanded = not self.branch_id_expanded
+	def toggle_product_id(self):
 
-	def switch_add_and_update(self):
-		if self.update_and_add_label.text() == "Update branch info? Click here":
+		current_height = self.product_id_line_edit.maximumHeight()
+		new_height = 0 if self.product_id_expanded else 40
+
+		self.product_id_line_edit.setMinimumHeight(new_height)
+		self.product_id_line_edit.setMaximumHeight(new_height)
+
+		self.product_id_expanded = not self.product_id_expanded
+
+	def switch_add_and_update_branch(self):
+		if self.branch_update_and_add_label.text() == "Update branch info? Click here":
 			# Currently in ADD mode, switch to UPDATE
-			self.update_and_add_label.setText("Add branch info")
+			self.branch_update_and_add_label.setText("Add branch info")
 			self.Manage_branch_info_label.setText("Update branch info")
 			self.update_and_add_button.setText("Update")
 		else:
 			# Currently in UPDATE mode, switch to ADD
-			self.update_and_add_label.setText("Update branch info? Click here")
+			self.branch_update_and_add_label.setText("Update branch info? Click here")
 			self.Manage_branch_info_label.setText("Add branch info")
 			self.update_and_add_button.setText("Add")
+	def switch_add_and_update_product(self):
+		if self.product_update_and_add_label.text() == "Update product info? Click here":
+			# Currently in ADD mode, switch to UPDATE
+			self.product_update_and_add_label.setText("Add product info")
+			self.Manage_product_info_label.setText("Update product info")
+			self.product_update_and_add_button.setText("Update")
+		else:
+			# Currently in UPDATE mode, switch to ADD
+			self.product_update_and_add_label.setText("Update product info? Click here")
+			self.Manage_product_info_label.setText("Add product info")
+			self.product_update_and_add_button.setText("Add")
 	def get_branchesForm_data(self):
 		return {
 			"id": self.branch_id_line_edit.text().strip(),
